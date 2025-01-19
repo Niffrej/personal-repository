6f411870914a22fed9281ca811f7e1ead8151342
Niffrej <franojerffeson@gmail.com>
2025-01-19 19:45:51 -0300

    estrutura incial

--- assets/js/components/thumbnails.js
+++ assets/js/components/thumbnails.js
@@ -2,7 +2,7 @@ const thumbnails = {
   'Delfino-eventos': 'assets/img/delfino-eventos.jpg',
   'desafio-Landing-Page': 'assets/img/desafio-Landing-Page.jpg',
   'estacionamento-jQuery': 'assets/img/estacionamento-jQuery.jpg',
-  'personal-repository': 'assets/img/default-thumbnail.png',
+  'personal-repository': 'assets/img/repository.jpg',
   'simple-ecommerce': 'assets/img/back-end.png',
   'simple-ecommerce-front': 'assets/img/simple-ecommerce-front.jpg',
 };
